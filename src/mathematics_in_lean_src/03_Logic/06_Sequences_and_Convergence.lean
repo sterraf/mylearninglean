8440ff8777c6ec4f95166d88a0cf88c19a15a647
Pedro Sánchez Terraf <pedrost@gmail.com>
2022-10-27 09:49:18 -0300

    Trying semicolon

--- src/mathematics_in_lean_src/03_Logic/06_Sequences_and_Convergence.lean
+++ src/mathematics_in_lean_src/03_Logic/06_Sequences_and_Convergence.lean
@@ -11,9 +11,8 @@ by  { congr, ring }
 
 example {a : ℝ} (h : 1 < a) : a < a * a :=
 begin
-  convert (mul_lt_mul_right _).2 h,
-  { rw [one_mul] },
-  exact lt_trans zero_lt_one h
+  convert (mul_lt_mul_right _).2 h;
+  linarith
 end
 
 theorem converges_to_const (a : ℝ) : converges_to (λ x : ℕ, a) a :=
