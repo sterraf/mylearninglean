ea4492a3b0d276ea6e635ac3cfbb521cf4aac8cd
Pedro Sánchez Terraf <pedrost@gmail.com>
2022-11-14 12:21:50 -0300

    Repeated argument

--- src/borel_hierarchy.lean
+++ src/borel_hierarchy.lean
@@ -11,7 +11,7 @@ section rec_gen
 universe u 
 parameter (X : Type*)
 variable (g : set (set X))
-variable {α : Type u}
+variables {α : Type u} (s : set (set α))
 
 @[reducible]
 noncomputable def ordω₁ := (aleph 1 : cardinal.{u}).ord
@@ -31,7 +31,8 @@ noncomputable def minω₁ : ω₁ :=  @enum ω₁ <ω₁ woω₁ 0
 lemma not_lt_min_omega1 (i : ω₁) : ¬(i < minω₁) :=
  not_lt_of_le (enum_zero_le' zero_lt_aleph_1_ord i)
 
-def sigma0_pi0_rec (s : set (set α)) : ω₁ → set (set α) × set (set α)
+@[protected]
+def sigma0_pi0_rec : ω₁ → set (set α) × set (set α)
 | i :=
   let 
     P_old := ⋃ j : Iio i, (sigma0_pi0_rec j.1).snd,
@@ -41,13 +42,13 @@ def sigma0_pi0_rec (s : set (set α)) : ω₁ → set (set α) × set (set α)
     ⟨S , P⟩
 using_well_founded {dec_tac := `[exact j.2]}
 
-def sigma0 (s : set (set α)) (i : ω₁) : set (set α) := (sigma0_pi0_rec s i).fst
+def sigma0 (i : ω₁) : set (set α) := (sigma0_pi0_rec s i).fst
 
-def pi0 (s : set (set α)) (i : ω₁) : set (set α) := (sigma0_pi0_rec s i).snd
+def pi0 (i : ω₁) : set (set α) := (sigma0_pi0_rec s i).snd
 
-theorem sigma0_pi0_rec_def' (s : set (set α)) (i : ω₁) : sigma0_pi0_rec s i = ⟨sigma0 s i, pi0 s i⟩ := by { unfold pi0 sigma0, simp }
+theorem sigma0_pi0_rec_def' (i : ω₁) : sigma0_pi0_rec s i = ⟨sigma0 s i, pi0 s i⟩ := by { unfold pi0 sigma0, simp }
 
-theorem pi0_sub_sigma0 (s : set (set α)) (i k : ω₁) (hik : i < k) : pi0 s i ⊆ sigma0 s k := 
+theorem pi0_sub_sigma0 (i k : ω₁) (hik : i < k) : pi0 s i ⊆ sigma0 s k :=
 begin
   have : i ∈ Iio k := by simp [hik],
   unfold sigma0 sigma0_pi0_rec,
@@ -62,7 +63,7 @@ begin
   exact Union_const x,
 end
 
-theorem self_subset_sigma0 (s : set (set α)) (i : ω₁) :
+theorem self_subset_sigma0 (i : ω₁) :
   s ⊆ sigma0 s i :=
 begin
   unfold sigma0 sigma0_pi0_rec,
@@ -70,7 +71,7 @@ begin
   exact subset_rfl
 end
 
-theorem compl_self_subset_pi0 (s : set (set α)) (i : ω₁) :
+theorem compl_self_subset_pi0 (i : ω₁) :
   compl '' s ⊆ pi0 s i :=
 begin
   unfold pi0 sigma0_pi0_rec, simp only,
@@ -79,14 +80,14 @@ begin
   exact subset_rfl
 end
 
-theorem empty_mem_sigma0 (s : set (set α)) (i : ω₁) :
+theorem empty_mem_sigma0 (i : ω₁) :
   ∅ ∈ sigma0 s i :=
 begin
   unfold sigma0 sigma0_pi0_rec, simp only,
   exact mem_union_left _ (mem_union_right _ (mem_singleton ∅))
 end
 
-theorem univ_mem_pi0 (s : set (set α)) (i : ω₁) : 
+theorem univ_mem_pi0 (i : ω₁) :
 set.univ ∈ pi0 s i := by { unfold pi0 sigma0_pi0_rec, simp }
 
 end rec_gen
