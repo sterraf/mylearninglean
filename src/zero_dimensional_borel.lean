47ed6c93b8d69a27505124611ccc3bc429cb0573
Pedro Sánchez Terraf <pedrost@gmail.com>
2023-01-06 11:21:30 -0300

    Watching for line length

--- src/zero_dimensional_borel.lean
+++ src/zero_dimensional_borel.lean
@@ -6,7 +6,7 @@ open topological_space
 
 namespace borel_classes
 
-open pointclasses
+open set pointclasses
 
 variables {α : Type*} [topological_space α] [second_countable_topology α]
 
@@ -23,32 +23,33 @@ lemma sigma0_one : sigma0 (countable_basis α) 1 = {u : set α | is_open u} :=
 begin
   ext z, refine ⟨λ hz, _, λ hz, _⟩,
   { rw pointclasses.sigma0_one at hz,
-    simp only [set.mem_range, set.mem_set_of_eq] at *,
+    simp only [mem_range, mem_set_of_eq] at *,
     rcases hz with ⟨y,rfl⟩,
     apply is_open_Union,
     intro i,
     rcases (y i).property with h | h | h,
     { exact is_open_of_mem_countable_basis h },
     { rw h, exact is_open_empty },
-    { simp only [set.mem_singleton_iff, subtype.val_eq_coe] at *,
+    { simp only [mem_singleton_iff, subtype.val_eq_coe] at *,
       rw h, exact is_open_univ } },
-  { rw [set.mem_set_of_eq,
+  { rw [mem_set_of_eq,
       is_topological_basis.open_iff_eq_sUnion (is_basis_countable_basis α)] at hz,
     rcases hz with ⟨S,hS,hz⟩,
     rw pointclasses.sigma0_one,
     rcases classical.em (S=∅) with rfl | nonemp,
-    { existsi λ n, (⟨∅,_⟩ : ↥(countable_basis α ∪ {∅, set.univ})),
-      { simp only [set.Union_empty],
-        rw set.sUnion_empty at hz,
+    { existsi λ n, (⟨∅,_⟩ : ↥(countable_basis α ∪ {∅, univ})),
+      { simp only [Union_empty],
+        rw sUnion_empty at hz,
         exact hz.symm },
-      { simp only [set.mem_insert_iff, true_or, eq_self_iff_true, set.union_insert, set.union_singleton] } },
-    { rw set.sUnion_eq_Union at hz,
-      have Sc : S.countable := set.countable.mono hS (countable_basis α).to_countable,
-      have gsurj : ∃ (g : ℕ → S), g.surjective := (set.countable_iff_exists_surjective (set.nonempty_iff_ne_empty.mpr nonemp)).mp Sc,
+      { simp only [mem_insert_iff, true_or, eq_self_iff_true, union_insert, union_singleton] } },
+    { rw sUnion_eq_Union at hz,
+      have Sc : S.countable := countable.mono hS (countable_basis α).to_countable,
+      have gsurj : ∃ (g : ℕ → S), g.surjective :=
+        (set.countable_iff_exists_surjective (nonempty_iff_ne_empty.mpr nonemp)).mp Sc,
       cases gsurj with g hg,
-      use set.inclusion (set.subset_union_of_subset_left hS {∅, set.univ}) ∘ g,
-      simp only [subtype.val_eq_coe, set.coe_inclusion],
-      rw ← set.Union_congr_of_surjective g hg at hz,
+      use inclusion (subset_union_of_subset_left hS {∅, univ}) ∘ g,
+      simp only [subtype.val_eq_coe, coe_inclusion],
+      rw ← Union_congr_of_surjective g hg at hz,
       swap 3,
       exacts [λ n, (g n).val, hz.symm, λ n, eq.refl (g n)] } }
 end
@@ -87,7 +88,8 @@ variables {H : Type*} [topological_space H] [locally_compact_space H] [t2_space 
 
 /- Application of the random lemma, now using an instance -/
 
-instance zero_dim_space_of_totally_disconnected [totally_disconnected_space H] : zero_dim_space H := ⟨loc_compact_Haus_tot_disc_of_zero_dim⟩
+instance zero_dim_space_of_totally_disconnected [totally_disconnected_space H] : 
+  zero_dim_space H := ⟨loc_compact_Haus_tot_disc_of_zero_dim⟩
 
 theorem loc_compact_t2_tot_disc_iff_tot_sep' :
   totally_disconnected_space H ↔ totally_separated_space H :=
