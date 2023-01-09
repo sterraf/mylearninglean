102596d1aabe032f52a0ff0ae6948bb0d2206adf
Pedro Sánchez Terraf <pedrost@gmail.com>
2023-01-09 14:46:21 -0300

    Sync with mathlib

--- src/borel_hierarchy.lean
+++ src/borel_hierarchy.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2022 Pedro Sánchez Terraf. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Author: Pedro Sánchez Terraf.
+Authors: Pedro Sánchez Terraf
 -/
 import set_theory.cardinal.continuum
 import measure_theory.measurable_space_def
@@ -12,9 +12,32 @@ import tactic.induction
 /-!
 # The Borel hierarchy
 
-In this file we define recursively define the classical Borel hierarchy of sets.
-It is developed as a purely set-theoretic definition here, and it is used to reprove
-the result on the cardinal bound for σ-algebras, previously formalized by S. Gouëzel.
+In this file we recursively define the classical Borel hierarchy of sets.
+It is developed in purely set-theoretic terms, as a way to generate
+$\sigma$-algebras of sets.
+
+## Mathematical overview
+
+For a second countable topological space $X$, the Borel hierarchy is defined
+recursively as follows (viz., Sect. 11.B in [kechris1995]):
+$$
+\begin{align*}
+  \mathbf{\Sigma}^0_1(X)     &= \{U \subseteq X : U \text { is open }\} \\
+  \mathbf{\Pi}^0_{\xi}(X)    &= \{ X \setminus Q : Q \in \mathbf{\Sigma}^0_{\xi}(X)\} \\
+  \mathbf{\Sigma}^0_{\xi}(X) &= \left\{\textstyle\bigcup_n A_n : A_n \in
+    \mathbf{\Pi}^0_{\xi_n}(X), \xi_n<\xi, n \in \mathbb{N}\right\}, \text { if } \xi>1,
+\end{align*}
+$$
+where $\xi$ and $\xi_n$ are ordinals between $1$ and $\omega_1$.
+
+This is a streamlined version of the notation for classes of *definable* subsets
+of the space $X$ (aka, **pointclasses**): $\mathbf{\Pi}^0_1(X)$ are the closed sets;
+$\mathbf{\Sigma}^0_2(X)$ are the $F_\sigma$ sets; $\mathbf{\Pi}^0_2(X)$, the $G_\delta$;
+and so forth. This is especially useful for infinite indices $\xi$.
+
+In this file, we provide an inductive definition of the above hierarchy of subsets
+of an arbitrary type, without assuming a topology but using an extra parameter `s`
+which is intended to be a countable base of a topology.
 
 ## Main definitions
 
@@ -24,21 +47,19 @@ using its `bool` argument.
 
 ## Implementation notes
 
-Traditionally, pointclasses $\Sigma^0_\alpha$ and $\Pi^0_\alpha$ for positive
+Traditionally, pointclasses $\Sigma^0_\alpha$ and $\Pi^0_\alpha$ are defined for positive
 values of the ordinal $\alpha$. Here the definition is extended in such a way that
 $\Sigma^0_0 = \emptyset$ (which results in more generality for some lemmas) and
 $\Pi^0_0$ coincides with the set of generators.
 
-The `inductive` definition of `sigma0_pi0_rec` was suggested by Junyan Xu.
-
 ## References
 
-The definition of the hierarchy was extracted from [Kechris].
+The definition of the hierarchy was extracted from [kechris1995].
 -/
 
-universe u 
+universe u
 
-namespace pointclasses
+namespace pointclass
 
 section sigma0_pi0
 
@@ -53,10 +74,16 @@ variables {α : Type u} (s : set (set α)) (i k : ordinal.{u})
 
 /--
 Simultaneous recursive definition of Σ⁰ᵢ and Π⁰ᵢ pointclasses by recursion
-on ordinals (a variant of 11.B in Kechris, [Classical Descriptive Set Theory][Kechris]).
+on ordinals (a variant of 11.B in Kechris, [Classical Descriptive Set Theory][kechris1995]).
 
-The main difference is that the hierarchy starts at level 0: Π⁰₀ are intended to
-be basic open sets (augmented with `∅` and `univ`) and Σ⁰₀ is the empty family.
+The definition is encoded as a single inductive predicate, where the `bool` argument
+determines if we are defining Σ⁰ᵢ (for `ff`) or Π⁰ᵢ (for `tt`). The parameter
+`s : set (set α)` is the generating family.
+
+The main difference with the classical definition is that this hierarchy starts
+at level 0: Π⁰₀ coincides with `s`
+augmented with `∅` and `univ` (intended to be a countable basis of a topology) and
+Σ⁰₀ is the empty family.
 -/
 inductive sigma0_pi0_rec {α : Type u} (s : set (set α)) :
   ordinal.{u} → bool → set α → Prop
@@ -68,84 +95,77 @@ inductive sigma0_pi0_rec {α : Type u} (s : set (set α)) :
     (∀ n, g n < i) → (∀ n, sigma0_pi0_rec (g n) tt (f n)) → sigma0_pi0_rec i ff (⋃ n, f n)
 
 /--
-The family of (boldface) Σ⁰ᵢ pointsets, which are countable unions of Π⁰ⱼ sets (given by the function `pointclasses.pi0` below) of smaller index. 
+The family of (boldface) Σ⁰ᵢ pointsets, which are countable unions of Π⁰ⱼ sets
+(given by the function `pointclass.pi0` below) of smaller index. The parameter
+`s : set (set α)` is the family of sets from which the generation begins.
 -/
 def sigma0 : set (set α) := sigma0_pi0_rec s i ff
 
 /--
-The family of (boldface) Π⁰ᵢ pointsets, which are the complements of Π⁰ᵢ sets (given by the function `pointclasses.sigma0` above).
-When the ordinal argument is `0`, it returns the generating family `s`.
+The family of (boldface) Π⁰ᵢ pointsets, which are the complements of Σ⁰ᵢ sets
+(given by the function `pointclass.sigma0` above). The parameter
+`s : set (set α)` is the family of sets from which the generation begins.
+
+When the ordinal argument is `0`, `pi0` returns the generating family `s`
+augmented with `∅` and `univ`.
 -/
 def pi0 : set (set α) := sigma0_pi0_rec s i tt
 
 lemma sigma0_pi0_rec_def' {b : bool} :
   sigma0_pi0_rec s i b = if b then pi0 s i else sigma0 s i :=
-by { unfold pi0 sigma0, cases b; refl }
+by { cases b; refl }
 
-@[simp]
-lemma sigma0_zero :
-  sigma0 s 0 = ∅ :=
-begin
-  unfold sigma0,
-  ext x, rw [mem_empty_iff_false, iff_false],
-  intro hx,
-  induction' hx with _ _ _ _ _ _ _ f g glt hf IH,
-  exact ordinal.not_lt_zero (g 0) (glt 0)
- end
-
-lemma sigma0_eq_Union_pi0:
+/-- `sigma0` comprises unions of previous `pi0`. -/
+lemma sigma0_eq_Union_pi0 :
   sigma0 s i = set.range (λ (f : ℕ → ⋃ j (hij : j < i), pi0 s j), ⋃ n, (f n).1) :=
 begin
-  rcases classical.em (i=0) with rfl | hi; unfold sigma0, rw sigma0_pi0_rec_def',
-  { apply eq.symm, simp [range_eq_empty, ordinal.not_lt_zero] },
-  { ext x, split; intro hx,
-    { induction' hx with _ _ _ _ _ _ i f g glt hf,
-      existsi λn, (⟨f n, _⟩ : ↥⋃ j < i, pi0 s j),
-      { simp only [eq_self_iff_true] },
-      { rw mem_Union,
+  ext x, split; intro hx,
+  { induction' hx with _ _ _ _ _ _ i f g glt hf,
+    existsi λ n, (⟨f n, _⟩ : ↥⋃ j < i, pi0 s j),
+    { simp only [eq_self_iff_true] },
+    { rw mem_Union,
       use g n,
       rw mem_Union,
       exact ⟨glt n, hf n⟩ } },
-    { cases hx with f hf,
-      rw ← hf,
-      dsimp only at hf,
-      choose g hg using λn, (mem_Union.mp (f n).property),
-      simp only [mem_Union,exists_prop] at hg,
-      apply sigma0_pi0_rec.union _ g,
-      exact λ n, (hg n).1,
-      unfold pi0 at hg,
-      exact λ n, (hg n).2 } }
+  { cases hx with f hf,
+    rw ← hf,
+    dsimp only at hf,
+    choose g hg using λ n, (mem_Union.mp (f n).property),
+    simp only [mem_Union, exists_prop] at hg,
+    exact sigma0_pi0_rec.union _ g (λ n, (hg n).1) (λ n, (hg n).2) }
 end
+
+/-- Base case for `sigma0`. -/
+@[simp] lemma sigma0_zero : sigma0 s 0 = ∅ :=
+by simp [sigma0_eq_Union_pi0, ordinal.not_lt_zero]
 
 lemma pi0_subset_sigma0 (hik : i < k) :
   pi0 s i ⊆ sigma0 s k :=
 begin
-  simp only [sigma0_eq_Union_pi0,hik],
+  simp only [sigma0_eq_Union_pi0, hik],
   intros x hx,
   apply mem_range.mpr,
   have hxU : x ∈ ⋃ j < k, pi0 s j,
-  { simp only [mem_Union,exists_prop],
-    use i,
-    exact ⟨hik,hx⟩ },
-  existsi (λn : ℕ, (⟨x,hxU⟩ : ⋃ (j < k), pi0 s j)),
-  exact Union_const x
+  { simp only [mem_Union, exists_prop],
+    exact ⟨i, hik, hx⟩ },
+  exact ⟨(λ n : ℕ, (⟨x, hxU⟩ : ⋃ (j < k), pi0 s j)), Union_const x⟩
 end
 
-lemma pi0_eq_compl_sigma0 (hi : ¬i = 0):
+/-- `pi0` comprises complements of `sigma0` at the same level. -/
+lemma pi0_eq_compl_sigma0 (hi : i ≠ 0) :
   pi0 s i = compl '' sigma0 s i :=
 begin
-  unfold sigma0 pi0,
   ext x, split; intro hx; induction' hx with hcomp IH c d IH,
   any_goals { contradiction },
   { simp only [mem_image], use d, tauto },
   { have := sigma0_pi0_rec.compl IH.1, rwa ← IH.2 }
-end  
+end
 
+/-- Base case for `pi0`. -/
 lemma pi0_zero :
-  pi0 s 0 = s ∪ {∅,univ} :=
+  pi0 s 0 = s ∪ {∅, univ} :=
 begin
-  unfold pi0, ext,
-  simp only [mem_insert_iff,union_insert,union_singleton],
+  ext x, simp only [mem_insert_iff, union_insert, union_singleton],
   split; intro hx,
   { induction' hx with _ hx _ v hv,
     any_goals { tauto },
@@ -160,9 +180,8 @@ begin
 end
 
 lemma sigma0_one :
-  sigma0 s 1 = set.range (λ (f : ℕ → s ∪ {∅,univ}), ⋃ n, (f n).1) :=
+  sigma0 s 1 = set.range (λ (f : ℕ → s ∪ {∅, univ}), ⋃ n, (f n).1) :=
 begin
-  unfold sigma0,
   change sigma0_pi0_rec s 1 ff = range (λ (f : ℕ → ↥(s ∪ {∅, univ})), ⋃ (n : ℕ), ↑(f n)),
   ext z,
   change sigma0_pi0_rec s 1 ff z ↔ ∃ (y : ℕ → ↥(s ∪ {∅, univ})), (⋃ (n : ℕ), ↑(y n)) = z,
@@ -173,21 +192,21 @@ begin
     simp_rw glt1 at IH,
     change ∀ n, f n ∈ pi0 s 0 at IH,
     simp_rw pi0_zero at IH,
-    use λ n, ⟨f n, IH n⟩,
-    exact rfl },
-  { rcases h with ⟨f,rfl⟩,
+    exact ⟨λ n, ⟨f n, IH n⟩, rfl⟩ },
+  { rcases h with ⟨f, rfl⟩,
     apply sigma0_pi0_rec.union (λ n, (f n).val) (λ n, 0),
     { simp },
     { change ∀ (n : ℕ), sigma0_pi0_rec s 0 tt ↑(f n),
       intro n,
       have hfn := (f n).property,
-      simp only [subtype.val_eq_coe,mem_insert_iff,union_insert,union_singleton] at hfn,
+      simp only [subtype.val_eq_coe, mem_insert_iff, union_insert, union_singleton] at hfn,
       rcases hfn with nul | uni | bas,
       { rw nul, exact sigma0_pi0_rec.empty },
       { rw uni, exact sigma0_pi0_rec.univ },
       { exact sigma0_pi0_rec.basic (f n) bas } } }
 end
 
+/-- `sigma0` is monotone. -/
 lemma sigma0_subset_sigma0 (hik : i ≤ k) :
   sigma0 s i ⊆ sigma0 s k :=
 begin
@@ -210,76 +229,75 @@ begin
         let (Exists.intro l r) := mem_Union.mp q in
         Exists.intro j (Exists.intro (trans l hik) r)),
     cases this with j hj,
-    use j,
-    exact mem_Union.mpr hj },
+    exact ⟨j, mem_Union.mpr hj⟩ },
   apply mem_range.mpr,
-  existsi (λn : ℕ, (⟨f n, hfUn n⟩ : ⋃ j < k, (pi0 s j))),
+  existsi (λ n : ℕ, (⟨f n, hfUn n⟩ : ⋃ j < k, (pi0 s j))),
   tauto,
 end
 
 /--
 The sequence of `pi0` families is nondecreasing.
 
-The hypothesis `¬i = 0` is required in case elements of the generating set are
+The hypothesis `i ≠ 0` is required in case elements of the generating set are
 not closed. If the underlying space is zero-dimensional, one can take a basis
-of clopen sets and the inclusion will hold unconditionally. 
+of clopen sets and the inclusion will hold unconditionally.
 -/
-lemma pi0_subset_pi0 (hi : ¬i = 0) (hik : i ≤ k) :
+lemma pi0_subset_pi0 (hi : i ≠ 0) (hik : i ≤ k) :
   pi0 s i ⊆ pi0 s k :=
 begin
-  rw [pi0_eq_compl_sigma0,pi0_eq_compl_sigma0],
+  rw [pi0_eq_compl_sigma0, pi0_eq_compl_sigma0],
   exacts [image_subset _ (sigma0_subset_sigma0 s i k hik),
     ordinal.one_le_iff_ne_zero.mp (trans (ordinal.one_le_iff_ne_zero.mpr hi) hik),
     hi]
 end
 
+/-- `sigma0` is closed under countable unions. -/
 lemma Union_of_sigma0_sequence {g : ℕ → sigma0 s i} :
   (⋃ n, (g n).val) ∈ sigma0 s i :=
 begin
   have hg : ∀ n : ℕ, (g n).val ∈ sigma0 s i := λ n, (g n).property,
-  simp only [subtype.val_eq_coe,sigma0_eq_Union_pi0] at *,
+  simp only [subtype.val_eq_coe, sigma0_eq_Union_pi0] at *,
   choose o ho using hg,
   have : ℕ × ℕ ≃ ℕ,
   { exact denumerable.eqv (ℕ × ℕ) },
   cases this with tup untup htup huntup,
   use λ n, let p := (untup n) in o p.1 p.2,
   ext x, split; intro hx; simp only [mem_Union] at hx ⊢;
-  cases hx with j hxin,
+    cases hx with j hxin,
   { let n := (untup j).fst,
     use n,
-    specialize ho n,
-    rw [← ho, mem_Union],
-    use (untup j).snd,
-    assumption },
-  { simp only [mem_Union,← ho j] at hxin,
+    rw [← ho n, mem_Union],
+    exact ⟨(untup j).snd, hxin⟩ },
+  { simp only [mem_Union, ← ho j] at hxin,
     cases hxin with k hk,
-    existsi tup ⟨j,k⟩,
-    have fstj : (untup (tup ⟨j,k⟩)).fst = j,
+    existsi tup ⟨j, k⟩,
+    have fstj : (untup (tup ⟨j, k⟩)).fst = j,
     { exact (congr_arg prod.fst (htup (j, k))).trans rfl },
-    have sndk : (untup (tup ⟨j,k⟩)).snd = k,
+    have sndk : (untup (tup ⟨j, k⟩)).snd = k,
     { exact (congr_arg prod.snd (htup (j, k))).trans rfl },
-    rw [fstj,sndk],
+    rw [fstj, sndk],
     exact hk }
 end
 
-lemma Union_of_mem_sigma0 {f : ℕ → set α} (hf : ∀ n, f n ∈ sigma0 s i):
+/-- Variant of `pointclass.Union_of_sigma0_sequence`. -/
+lemma Union_of_mem_sigma0 {f : ℕ → set α} (hf : ∀ n, f n ∈ sigma0 s i) :
   (⋃ n, f n) ∈ sigma0 s i :=
-by exact @Union_of_sigma0_sequence _ s i (λn, {val := f n, property := hf n} : ℕ → sigma0 s i)
+by exact @Union_of_sigma0_sequence _ s i (λ n, {val := f n, property := hf n} : ℕ → sigma0 s i)
 
-lemma self_subset_sigma0 (hi : ¬i = 0) :
+lemma self_subset_sigma0 (hi : i ≠ 0) :
   s ⊆ sigma0 s i :=
 begin
   calc
-  s   ⊆ s ∪ {∅,univ} : subset_union_left _ _
+  s   ⊆ s ∪ {∅, univ} : subset_union_left _ _
   ... = pi0 s 0      : eq.symm (pi0_zero s)
   ... ⊆ sigma0 s i   : pi0_subset_sigma0 s 0 i (ordinal.pos_iff_ne_zero.mpr hi),
 end
 
-theorem empty_mem_sigma0 (hi : ¬i = 0) :
+theorem empty_mem_sigma0 (hi : i ≠ 0) :
   ∅ ∈ sigma0 s i :=
 begin
-  have : ∅ ∈ s ∪ {∅,univ} := by { apply mem_union_right, simp },
-  have that : s ∪ {∅,univ} = pi0 s 0 := eq.symm (pi0_zero s),
+  have : ∅ ∈ s ∪ {∅, univ} := by { apply mem_union_right, simp },
+  have that : s ∪ {∅, univ} = pi0 s 0 := eq.symm (pi0_zero s),
   rw that at this,
   calc
   ∅   ∈ pi0 s 0      : this
@@ -310,10 +328,10 @@ begin
   apply subset_antisymm,
   { rw sigma0_eq_Union_pi0,
     intros x hx,
-    simp only [mem_Union,exists_prop] at *,
+    simp only [mem_Union, exists_prop] at *,
     cases hx with f hf,
     let g := λ n, (f n).property,
-    simp only [mem_Union,exists_prop] at g,
+    simp only [mem_Union, exists_prop] at g,
     choose o ho using g,
     use order.succ(sup o),
     split,
@@ -329,25 +347,24 @@ begin
     tauto },
   { simp only [Union_subset_iff],
     exact λ _ hi, sigma0_subset_sigma0 s _ _ (le_of_lt hi) }
-end 
+end
 
 theorem compl_mem_gen_measurable (t : set α) (ht : t ∈ gen_measurable s) :
   tᶜ ∈ gen_measurable s :=
 begin
   rw gen_measurable_eq_Union_sigma0 at ht,
-  simp only [mem_Union,exists_prop] at ht,
+  simp only [mem_Union, exists_prop] at ht,
   cases ht with o ho,
   rcases classical.em (o=0) with rfl | onon,
   { finish },
   calc
-  tᶜ  ∈ pi0 s o          : by { rw pi0_eq_compl_sigma0, 
-    simp only [mem_image,compl_inj_iff,exists_eq_right], exacts [ho.2,onon] }
+  tᶜ  ∈ pi0 s o          : by { rw pi0_eq_compl_sigma0,
+    simp only [mem_image, compl_inj_iff, exists_eq_right], exacts [ho.2, onon] }
   ... ⊆ gen_measurable s : pi0_subset_sigma0 s o ω₁ ho.1,
 end
 
 theorem Union_mem_gen_measurable {f : ℕ → set α} (hf : ∀ n, f n ∈ gen_measurable s) :
-  (⋃ n, f n) ∈ gen_measurable s:=
-by { unfold gen_measurable at *, exact Union_of_mem_sigma0 s ω₁ hf }
+  (⋃ n, f n) ∈ gen_measurable s := Union_of_mem_sigma0 s ω₁ hf
 
 open measurable_space
 
@@ -355,20 +372,20 @@ lemma generate_measurable_of_mem_sigma0 (t) (ht : t ∈ sigma0 s i) :
   generate_measurable s t :=
 begin
   induction i using ordinal.induction with i IH generalizing t,
-  rw [sigma0_eq_Union_pi0,mem_range] at ht,
-  rcases ht with ⟨f,hf⟩,
+  rw [sigma0_eq_Union_pi0, mem_range] at ht,
+  rcases ht with ⟨f, hf⟩,
   have typf : ∀ n : ℕ, generate_measurable s (f n),
   { intro n,
-    have fn_in : (f n).val ∈ ⋃ (j : ordinal) (hij : j < i), pi0 s j := (f n).property,
-    simp only [subtype.val_eq_coe,mem_Union,exists_prop] at fn_in,
-    rcases fn_in with ⟨o,⟨o_lt_i,fn_in⟩⟩,
+    have fn_in : (f n).val ∈ ⋃ j < i, pi0 s j := (f n).property,
+    simp only [subtype.val_eq_coe, mem_Union, exists_prop] at fn_in,
+    rcases fn_in with ⟨o, ⟨o_lt_i, fn_in⟩⟩,
     -- Case `(f n).val ∈ pi0 s 0`.
     rcases classical.em (o=0) with rfl | honz,
     { rw pi0_zero at fn_in,
       rcases fn_in with  fn_in | fn_emp | fn_in,
       { exact generate_measurable.basic _ fn_in },
       { rw fn_emp, exact generate_measurable.empty },
-      { rw mem_singleton_iff at fn_in, rw [fn_in,←compl_empty],
+      { rw mem_singleton_iff at fn_in, rw [fn_in, ←compl_empty],
         exact generate_measurable.compl _ generate_measurable.empty } },
     -- Case `(f n).val ∈ pi0 s o` with `o ≠ 0`.
     simp only at IH,
@@ -379,22 +396,22 @@ begin
     rw [←hx.2, compl_compl],
     exact IH o o_lt_i x hx.1 },
   rw ← hf,
-  exact generate_measurable.union (λn, (f n).val) typf
+  exact generate_measurable.union (λ n, (f n).val) typf
 end
 
-/-- `generate_measurable_rec s` generates precisely the smallest sigma-algebra containing `s`. -/
+/-- `gen_measurable s` generates precisely the smallest σ-algebra containing `s`. -/
 theorem generate_measurable_eq_gen_measurable :
   {t | generate_measurable s t} = gen_measurable s :=
 begin
   ext t, refine ⟨λ ht, _, λ ht, _⟩,
   { have om1_nonz : ω₁ ≠ 0,
-    { unfold omega_1, exact ne_zero_of_out_nonempty _,},
+    { unfold omega_1, exact ne_zero_of_out_nonempty _ },
     induction ht with u hu u hu IH f hf IH,
     exacts
-    [ self_subset_sigma0 s ω₁ om1_nonz hu,
+      [self_subset_sigma0 s ω₁ om1_nonz hu,
       empty_mem_sigma0 s ω₁ om1_nonz,
       compl_mem_gen_measurable s u IH,
-      Union_mem_gen_measurable s IH ] },
+      Union_mem_gen_measurable s IH] },
   { exact generate_measurable_of_mem_sigma0 s ω₁ t ht }
 end
 
@@ -502,4 +519,4 @@ cardinal_generate_measurable_le_continuum _
 
 end card_gen_measurable
 
-end pointclasses
+end pointclass
